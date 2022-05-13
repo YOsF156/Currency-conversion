a5ee4c8fb8a766bcf8c80632a10b130845f03619
YOsF156 <yosistar@gmail.com>
2022-05-13 13:24:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import InputAmount from './InputAmount'
 function App() {
+  const [fromAmount, setFromAmount] = useState(1)
+  const [toAmount, setToAmount] = useState(3.5)
+
+  const convert = (e) => {
+    if (e.target.value >= 0) {
+
+      if (e.target.id === "fromAmount") {
+        setFromAmount(e.target.value)
+        setToAmount((e.target.value * 3.5).toFixed(2))
+      } else {
+        setToAmount(e.target.value)
+        setFromAmount((e.target.value / 3.5).toFixed(2))
+      }
+    }
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        currency convector
       </header>
-    </div>
+      <div className="body">
+        <div className="inputs">
+
+          <InputAmount id="fromAmount" lbl={"USD"} value={fromAmount} convert={convert} pH={"type $ currency here"} />
+          <h4>
+            <strong  >
+              =
+            </strong>
+          </h4>
+          <InputAmount id="toAmount" lbl={"NIS"} value={toAmount} convert={convert} pH={'type ₪ currency here'} />
+
+        </div>
+      </div>
+    </div >
   );
 }
 
