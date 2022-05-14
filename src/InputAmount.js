08f380c1fdbeda06bfa6bde2e86817f1a05d49a6
YOsF156 <yosistar@gmail.com>
2022-05-14 20:56:56 +0300

    some css

--- src/InputAmount.js
+++ src/InputAmount.js
@@ -2,7 +2,7 @@ export default function inputAmount(props) {
 
     return (
         <div className="inputs">
-            <div>
+            <div className="input-row">
                 <label htmlFor={props.id}>{props.lbl} </label>
                 <input type="number" name={props.id} id={props.id} value={props.value} placeholder={props.pH} onChange={props.convert} />
             </div>
